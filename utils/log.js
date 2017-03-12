7020093d53e4e87716e28b599c0b48b097244384
Antoine Viau <contact@antoineviau.com>
2017-03-12 16:03:22 +0100

    Remise en route du proxy de logging

--- utils/log.js
+++ utils/log.js
@@ -1,9 +1,11 @@
+let http = require('http');
+
 module.exports = (txt) => {
     console.log(txt);
-    return;
-    let data = { from: global.nodeId, ts: (new Date().getTime()), "log": txt };
+//    return;
+    let data = { from: global.id, ts: (new Date().getTime()), "log": txt };
     let req = http.request(
-        { host: '127.0.0.1', port: '2501', method: 'POST', headers: { 'Content-Type': 'application/json' } });
+        { host: '127.0.0.1', port: 4001, method: 'POST', headers: { 'Content-Type': 'application/json' } });
     req.write(JSON.stringify(data));
     req.end();
 };
--- utils/proxy.js
+++ utils/proxy.js
@@ -1,29 +1,39 @@
 
-var http = require('http');
+let http = require('http');
+let static = require('node-static');
 
-var logs = [];
-var lastSend = 0;
+let logs = [];
+let lastSend = 0;
+let inputPort = 4001;
+let outputPort = 4002;
 
 // Input server
-http.createServer( (req, res)  => {
+http.createServer((req, res) => {
     var body = [];
-    req.on('data',  (chunk) => {
+    req.on('data', (chunk) => {
         body.push(chunk);
-    }).on('end',  ()=>  {
+    }).on('end', () => {
         body = Buffer.concat(body).toString();
         logs.push(JSON.parse(body));
-        console.log('Received : '+body);
+        console.log('Received : ' + body);
     });
-}).listen(2501);
+}).listen(inputPort);
+console.log('Input server is listenning on ' + inputPort);
 
 // Output server
-http.createServer( (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin','*')
-    res.writeHead(200, { 'Content-Type': 'application/json' });    
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(logs.filter(log => log.ts > lastSend)));
     console.log('Sent : ', logs);
     lastSend = (new Date()).getTime();
-}).listen(2502);
- 
+}).listen(outputPort);
+console.log('Input server is listenning on ' + outputPort);
 
-
+var file = new static.Server('./utils');
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+console.log('Web server listenning on 8080');
