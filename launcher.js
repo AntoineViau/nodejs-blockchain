0dbf682df3e09feb0c4cc998fb932cd95e04fd92
Antoine Viau <contact@antoineviau.com>
2017-03-13 08:48:24 +0100

    Le launcher launch à intervalles irréguliers

--- launcher.js
+++ launcher.js
@@ -1,18 +1,20 @@
 var spawn = require('child_process').spawn;
 
-var prcs = (new Array(3)).fill(null).map((prc, index) => spawn('node', ['node.js', index]));
 
-prcs.forEach(prc => {
+function launch(id) {
+    let prc = spawn('node', ['peer.js', id]);
     prc.stdout.setEncoding('utf8');
     prc.stdout.on('data', function (data) {
         var str = data.toString()
         var lines = str.split(/(\r?\n)/g);
-        console.log(lines.join(""));
+        console.log(lines.join("").trim());
     });
-
     prc.on('close', function (code) {
         console.log('process exit code ' + code);
     });
+}
+
+launch(0);
+(new Array(3)).fill(null).map((val, index) => {
+    setTimeout(() => launch(index + 1), Math.floor(Math.random() * 5000))
 });
-
-
